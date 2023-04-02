58b2523492c20d819345eb59d7dff1926ca6f766
Eugene Koontz <ekoontz@hiro-tan.org>
2023-04-02 17:17:07 -0400

    create build scripts

---
+++ build-from-ubuntu-20-part-1.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+sudo apt-get update
+wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/cuda-keyring_1.0-1_all.deb
+sudo dpkg -i cuda-keyring_1.0-1_all.deb
+sudo add-apt-repository "deb https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/ /"
+sudo apt-cache search cuda
+sudo apt-cache search cuda | grep 11
+sudo apt-get install -y cuda-11-8
+sudo reboot # necessary to load drivers
+
---
+++ build-from-ubuntu-20-part-2.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+sudo apt-get install -y libgoogle-perftools-dev libprotobuf-c-dev protobuf-c-compiler libboost-system-dev gcc g++ cmake git
+wget -qO- "https://apt.repos.intel.com/intel-gpg-keys/GPG-PUB-KEY-INTEL-SW-PRODUCTS-2019.PUB" | sudo apt-key add -
+sudo sh -c "echo deb https://apt.repos.intel.com/mkl all main > /etc/apt/sources.list.d/intel-mkl.list"
+sudo apt-get update -o Dir::Etc::sourcelist="/etc/apt/sources.list.d/intel-mkl.list"
+sudo apt-get install -y --no-install-recommends intel-mkl-64bit-2020.0-088
+git clone https://github.com/marian-nmt/marian
+cd marian/
+mkdir build
+CC=/usr/bin/gcc-9 CXX=/usr/bin/g++-9 CUDAHOSTCXX=/usr/bin/g++-9 cmake ..  \
+  -DBoost_ARCHITECTURE=-x64 \
+  -DCMAKE_BUILD_TYPE=Slim \
+  -DCOMPILE_CPU=on \
+  -DCOMPILE_CUDA=true \
+  -DCOMPILE_KEPLER=true \
+  -DCOMPILE_MAXWELL=true \
+  -DCOMPILE_SERVER=on \
+  -DCUDA_TOOLKIT_ROOT_DIR=/usr/local/cuda-11 \
+  -DUSE_FBGEMM=false \
+  -DUSE_SENTENCEPIECE=on \
+  -DUSE_STATIC_LIBS=on
+make -j4
